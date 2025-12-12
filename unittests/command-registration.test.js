9dfbd3b75a2c9d6d69e39ca487e439444296d299
Destroyer-Sec <destroyeranon@gmail.com>
2025-12-12 09:57:07 -0800

    Remove deprecated unit test files after migration to the extension package

--- unittests/command-registration.test.js
+++
@@ -1,4 +0,0 @@
-// Deprecated: unit tests moved under the extension package at:
-//   org-vscode/org-vscode/test/unit
-
-module.exports = require('../org-vscode/test/unit/command-registration.test.js');
--- unittests/run.js
+++
@@ -1,5 +0,0 @@
-// Deprecated: unit tests moved under the extension package at:
-//   org-vscode/org-vscode/test/unit
-// Keep this file as a shim so older docs/commands still work.
-
-require('../org-vscode/test/unit/run.js');
